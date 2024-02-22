c63318c71532510bdf2734bc5bb78a24609d26d3
yang <zy892065502@gmail.com>
2024-02-22 16:03:41 +0800

    fix stream bug

--- src/pages/api/stream.ts
+++ src/pages/api/stream.ts
@@ -2,20 +2,27 @@ import {
     ParsedEvent,
     ReconnectInterval,
     createParser,
-  } from 'eventsource-parser';
-  import { OpenAIStream, StreamingTextResponse } from 'ai';
-
+} from 'eventsource-parser';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+export const config = {
+    runtime: 'edge',
+};
 
 export default async function handler(
-    req,
+    req: Request,
     res
 ) {
     if (req.method === 'POST') {
-        try{
-            const { model, messages, temperature } = req.body;
-            let url = `${process.env.OPENAI_BASE_URL}/chat/completions`;
+        try {
+            let body = await req.json();
+            const { model, messages, temperature } = body;
+            console.log(body);
+            // console.log(messages);
+            let url = `${process.env.OPENAI_BASE_URL}chat/completions`;
+            // let url = `https://ai-yyds.com/v1/chat/completions`;
             const key = process.env.OPENAI_API_KEY
-            if(url==null || key == null){
+            // const key = 'sk-JGZVAOI7Z4bt5zo8Ff23F09405Cd4b2386B177A42fB48a21'
+            if (url == null || key == null) {
                 console.log(`NO OPENAI_BASE_URL or OPENAI_API_KEY`);
             }
             const res = await fetch(url, {
@@ -25,50 +32,52 @@ export default async function handler(
                 },
                 method: 'POST',
                 body: JSON.stringify({
-                    model: model? model : 'gpt-3.5-turbo',
+                    model: model ? model : 'gpt-3.5-turbo',
                     messages: messages,
                     max_tokens: 1000,
-                    temperature: temperature? temperature : 0.1,
+                    temperature: temperature ? temperature : 0.1,
                     stream: true,
                 }),
             });
-              // Convert the response into a friendly text-stream
-                const stream = OpenAIStream(res);
-                // Respond with the stream
-                return new StreamingTextResponse(stream);
-            // const encoder = new TextEncoder();
-            // const decoder = new TextDecoder();
-            // const stream = new ReadableStream({
-            //     async start(controller) {
-            //         const onParse = (event: ParsedEvent | ReconnectInterval) => {
-            //             if (event.type === 'event') {
-            //                 const data = event.data;
-            //                 try {
-            //                     const json = JSON.parse(data);
-            //                     if (json.choices[0].finish_reason != null) {
-            //                         controller.close();
-            //                         return;
-            //                     }
-            //                     const text = json.choices[0].delta.content;
-            //                     const queue = encoder.encode(text);
-            //                     controller.enqueue(queue);
-            //                 } catch (e) {
-            //                     controller.error(e);
-            //                 }
-            //             }
-            //         };
-    
-            //         const parser = createParser(onParse);
-    
-            //         for await (const chunk of res.body as any) {
-            //             parser.feed(decoder.decode(chunk));
-            //         }
-            //     },
-            // });
-    
-            // return new Response(stream);
-        } catch(error){
-            res.status(500).json({ error});
+
+            // const stream = OpenAIStream(res);
+            // return new StreamingTextResponse(stream);
+
+
+            const encoder = new TextEncoder();
+            const decoder = new TextDecoder();
+            const stream = new ReadableStream({
+                async start(controller) {
+                    const onParse = (event: ParsedEvent | ReconnectInterval) => {
+                        if (event.type === 'event') {
+                            const data = event.data;
+                            try {
+                                const json = JSON.parse(data);
+                                if (json.choices[0].finish_reason != null) {
+                                    controller.close();
+                                    return;
+                                }
+                                const text = json.choices[0].delta.content;
+                                const queue = encoder.encode(text);
+                                controller.enqueue(queue);
+                            } catch (e) {
+                                controller.error(e);
+                            }
+                        }
+                    };
+
+                    const parser = createParser(onParse);
+
+                    for await (const chunk of res.body as any) {
+                        parser.feed(decoder.decode(chunk));
+                        // console.log(decoder.decode(chunk));
+                    }
+                },
+            });
+
+            return new Response(stream);
+        } catch (error) {
+            res.status(500).json({ error });
         }
     } else {
         // 处理非POST请求
